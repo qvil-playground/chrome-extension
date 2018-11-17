a8c84ffc276c10647237fb5d76f09cfb017a14bd
Taesu Hyeon <qvil1127@gmail.com>
2018-11-18 00:00:39 +0900

    Remove unnecessary div tag

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
 class App extends Component {
   render() {
     return (
-      <div>
-        <SimpleCard />
-      </div>
+      <SimpleCard />
     );
   }
 }
